b8d2fce37ca72e97753bf93ae236b99258f2ad1e
sadik-20 <71242314+sadik-20@users.noreply.github.com>
2020-10-11 03:01:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,96 +1,71 @@
+
 import os
 import csv
-from pathlib import Path 
+from pathlib import Path
+input_file = Path("classrepo","python-challenge", "Pybank", "budget_data.csv")
 
-# Assign file location with the pathlib library
-csv_file_path = Path("PyPoll", "election_data.csv")
+# Create empty lists to
 
-# Declare Variables 
-total_votes = 0
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0
+total_months = []
+total_profit = []
+monthly_profit_change = []
+ 
+with open ('PyBank/budget_data.csv') as csv_file:
 
+     # Store the contents of budget_data.csv in the variable csvreader
+    csvreader = csv.reader(csv_file,delimiter=",") 
 
-with open(csv_file_path,newline = "", encoding="utf-8") as elections:
+    # Skip the header labels 
+    header = next(csvreader)  
 
-    # Store data under the csvreader variable
-    csvreader = csv.reader(elections,delimiter=",") 
-
-    # Skip the header 
-    header = next(csvreader)     
-
-    # Iterate through each row in the csv
+    # Iterate through the rows in the stored file contents
     for row in csvreader: 
 
-        # The actual index(voter size been told by TA- suppossed to be 3521001
-        # excel file some how doesn't have the exact number of voters. last row is 1048575 suppose to be 3521001)
-        # Count the unique Voter ID's and store in variable  called total_votes
-        total_votes +=1
+        # append the total months and total profit to their corresponding lists
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
 
-        # We have four candidates if the name is found, count the times it appears and store in a list
-        # We can use this values in our percent vote calculation in the print statements
-        if row[2] == "Khan": 
-            khan_votes +=1
-        elif row[2] == "Correy":
-            correy_votes +=1
-        elif row[2] == "Li": 
-            li_votes +=1
-        elif row[2] == "O'Tooley":
-            otooley_votes +=1
+    # Iterate through the profits in order to get the monthly change in profits
+    for i in range(len(total_profit)-1):
+        
+        # Take the difference between two months and append to monthly profit change
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+        
+# Obtain the max and min of the the montly profit change list
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
 
- # To find the winner we want to make a dictionary out of the two lists we previously created 
-candidates = ["Khan", "Correy", "Li","O'Tooley"]
-votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+# i plus 1 at the end since month associated with change is the + 1 month or next month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
 
-# the winner using a max function from the dictionary 
-dict_candidates_and_votes = dict(zip(candidates,votes))
-key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+# Print Statements
 
-# Print a the summary of the analysis
-khan_percent = (khan_votes/total_votes) *100
-correy_percent = (correy_votes/total_votes) * 100
-li_percent = (li_votes/total_votes)* 100
-otooley_percent = (otooley_votes/total_votes) * 100
-
-# Print the summary table
-print(f"Election Results")
-print(f"----------------------------")
-print(f"Total Votes: {total_votes}")
-print(f"----------------------------")
-print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
-print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
-print(f"Li: {li_percent:.3f}% ({li_votes})")
-print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-print(f"----------------------------")
-print(f"Winner: {key}")
-print(f"----------------------------")
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
 
 # Output files
-# Assign output file location and with the pathlib library
-output_file = Path( "PyPoll", "Election_Results_Summary.txt")
-with open(output_file,"w") as file:
+output_file = Path("python-challenge","PyBank", "Financial_Analysis_Summary.txt")
 
-# Write methods to print to Elections_Results_Summary 
-    file.write(f"Election Results")
+with open(output_file,"w") as file:    
+# Write methods to print to Financial_Analysis_Summary 
+    file.write("Financial Analysis")
     file.write("\n")
-    file.write(f"----------------------------")
+    file.write("----------------------------")
     file.write("\n")
-    file.write(f"Total Votes: {total_votes}")
+    file.write(f"Total Months: {len(total_months)}")
     file.write("\n")
-    file.write(f"----------------------------")
+    file.write(f"Total: ${sum(total_profit)}")
     file.write("\n")
-    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
     file.write("\n")
-    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
     file.write("\n")
-    file.write(f"Li: {li_percent:.3f}% ({li_votes})")
-    file.write("\n")
-    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
-    file.write("\n")
-    file.write(f"----------------------------")
-    file.write("\n")
-    file.write(f"Winner: {key}")
-    file.write("\n")
-    file.write(f"----------------------------")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+    
